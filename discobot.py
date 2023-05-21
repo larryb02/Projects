adb876a2b38d6daba5f7cfd7b404fbe9df5af139
larryb02 <77469185+larryb02@users.noreply.github.com>
2023-05-21 13:43:34 -0400

    Update discobot.py

--- discobot.py
+++ discobot.py
@@ -1,18 +1,64 @@
 import discord
+from discord.ext import commands
 import os
-import random
 from dotenv import load_dotenv
 import logging
+from discord import app_commands
+#import handleMessages
 
-load_dotenv()
+file = 'bot.env'
+load_dotenv(file)
 logging.basicConfig(level=logging.INFO)
-client = discord.Bot()
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='>', intents=intents)
+tree = app_commands.CommandTree(client)
+
 token = os.getenv('TOKEN')
+
+
+
 #establish connection to discord api/server
 @client.event
-
 async def on_ready():
-    print('I am Larvin and I am alive!')
+    print(f'{client.user} is online')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        handle(message.content)
+    #create slash commands
+     
+    
+client.run(token)
+
+def handle(message):
+    return
+
+
+     
+
+
+
+
+
+     
+     
+
+
+
+
+
+
+
+
+
+
+    
+
    
 
 

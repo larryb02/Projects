e3e1b11868474467d45a113476d1ff0782686183
larryb02 <larrykbanks21@gmail.com>
2023-05-26 13:51:29 -0400

    modified:   discobot.py

--- discobot.py
+++ discobot.py
@@ -10,17 +10,15 @@ load_dotenv(file)
 logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix='>', intents=intents)
+client = commands.Bot(command_prefix='$', intents=intents)
 
 token = os.getenv('TOKEN')
 
 
 @client.command()
-async def command(ctx):
-    pass
-
-
-#establish connection to discord api/server
+async def play(ctx,song_name):
+    await ctx.send("Playing" + song_name)
+    
 @client.event
 async def on_ready():
     await client.tree.sync()

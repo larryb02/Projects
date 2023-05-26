7078e8c11130185d6a4e758e65d575a471193042
larryb02 <larrykbanks21@gmail.com>
2023-05-26 14:19:23 -0400

    modified:   discobot.py

--- discobot.py
+++ discobot.py
@@ -3,21 +3,22 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import logging
-#import handleMessages
+
 
 file = 'bot.env'
 load_dotenv(file)
 logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix='$', intents=intents)
+client = commands.Bot(command_prefix=commands.when_mentioned_or('$'), intents=intents)
 
 token = os.getenv('TOKEN')
 
 
 @client.command()
-async def play(ctx,song_name):
-    await ctx.send("Playing" + song_name)
+async def play(ctx, *,song_name):
+    await ctx.send("Playing " + song_name)#will replace the parameter with the
+                                            #function that gets the song link
     
 @client.event
 async def on_ready():
@@ -29,6 +30,8 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         pass
+
+    await client.process_commands(message)
 
      
     

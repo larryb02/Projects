1a0c7142dd357d525f2267d9ed1c9338ddcd696c
larryb02 <77469185+larryb02@users.noreply.github.com>
2023-06-10 20:37:14 -0400

    Update discobot.py

--- discordbot/discobot.py
+++ discordbot/discobot.py
@@ -9,10 +9,14 @@ file = 'bot.env'
 load_dotenv(file)
 token = os.getenv('TOKEN')
 
-class Larvin:
+class Larvin(commands.Cog):
     def __init__(self):
+        self.intents = discord.Intents.default()
+        self.intents.message_content = True
+        self.client = commands.Bot(command_prefix=commands.when_mentioned_or('$'),intents = self.intents)
         pass
-    
+    @commands.Cog.listener()
+
 
 
 

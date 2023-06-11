fd2e5cd312f8cc11f9240320f604b5441b3fa83f
larryb02 <larrykbanks21@gmail.com>
2023-06-11 03:48:24 -0400

    deleted:    discordbot/discobot.py 	modified:   discordbot/youtubeStuff.py

--- discordbot/youtubeStuff.py
+++ discordbot/youtubeStuff.py
@@ -7,12 +7,22 @@ class Larvin(commands.Cog):
         self.intents.message_content = intents.message_content
         self.client = client
         
-    @commands.Cog.listener()
-    async def filler(ctx):
+    @commands.command()
+    async def play(ctx,song_name): #takes user input, gets song, bot joins channel that user is in, then streams audio
         pass
-
-   
-
+    @commands.command()
+    async def pause(ctx):
+        pass
+    
+    @commands.command()
+    async def skip(ctx):
+        pass
+    
+    @commands.command()
+    async def queue(ctx): #shows list of songs in the queue
+        pass
+    
+    @commands.Cog.listener()
     async def on_ready(self):
         print(f'{self.client.user} has logged in')
         pass
